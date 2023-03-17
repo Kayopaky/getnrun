b12c23a530e01befc35d1b54d92eea4ddbb0b368
Allen <ungosallen504@gmail.com>
2023-03-17 23:21:06 +0800

    try lang

---
+++ lib/Sheeesh.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class safafaf extends StatefulWidget {
+  const safafaf({Key? key}) : super(key: key);
+
+  @override
+  State<safafaf> createState() => _safafafState();
+}
+
+class _safafafState extends State<safafaf> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
